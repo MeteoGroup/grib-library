bca42075b37227525be377bbbbce7ad28d680fab
pauw <martijn@weer.nl>
2015-10-27 10:35:58 +0100

    Lombok

---
+++ src/main/java/org/meteogroup/grib_library/grib2/model/Grib2ID.java
@@ -0,0 +1,25 @@
+package org.meteogroup.grib_library.grib2.model;
+
+/**
+ * Created by roijen on 19-Oct-15.
+ */
+public class Grib2ID {
+
+    private int length;
+    
+    private short id;
+    private int centreId;
+    private int subCenterId;
+    private short tableVersion;
+    private short localTableVersionNumber;
+    private short referenceTime;
+    private int year;
+    private short month;
+    private short day;
+    private short hour;
+    private short minute;
+    private short second;
+    private short productionStatus;
+    private short typeOfData;
+
+}
---
+++ src/main/java/org/meteogroup/grib_library/grib2/model/Record.java
@@ -0,0 +1,38 @@
+package org.meteogroup.grib_library.grib2.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 
+ * @author Pauw
+ *
+ */
+@Getter
+@Setter
+public class Record {
+
+//	private Grib2DRS drs = new null;
+//	
+//
+//	
+	private Grib2ID ids = null;
+//	
+//	private Grib2PDS grib1pds = null;
+//	
+//	private Grib2PDS pds = null;
+//
+//    private GDS gds = null;
+//    
+//    private Grib1BDS bds = null;
+//    
+//    private Grib2LUS lus = null;
+//    
+//    private Grib2BMS bms = null;
+//    
+//    private Grib2DS ds = null;
+//    
+//    private Grib2Endsection end = null;
+    
+	private int length = 0;
+}
