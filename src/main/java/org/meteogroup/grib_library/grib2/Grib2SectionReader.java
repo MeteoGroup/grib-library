35410c9e51789c9cc2ec3cef08760bf73e22cd7f
pauw <martijn@weer.nl>
2015-10-29 09:20:45 +0100

    Grib2 reading first stuff

--- src/main/java/org/meteogroup/grib_library/grib2/Grib2SectionReader.java
+++ src/main/java/org/meteogroup/grib_library/grib2/Grib2SectionReader.java
@@ -1,9 +1,21 @@
-package org.meteogroup.grib_library.grib1.model;
+package org.meteogroup.grib_library.grib2;
+
+import org.meteogroup.grib_library.exception.BinaryNumberConversionException;
+import org.meteogroup.grib_library.util.BytesToPrimitiveHelper;
 
 public class Grib2SectionReader {
+
+	private static final int POSITION_SECTIONNUMBER = 4;
 	
-	protected int readSectionLength(byte[] bytes){
-		return -1;
+	protected int readSectionLength(byte[] bytes, int headerOffSet) throws BinaryNumberConversionException {
+
+		return BytesToPrimitiveHelper.bytesToInteger(bytes[0], bytes[1],
+				bytes[2], bytes[3]);
+
+	}
+	
+	protected int readSectionNumber(byte[] bytes, int headerOffset){
+		return bytes[POSITION_SECTIONNUMBER]&0xFF;
 	}
 
 }
