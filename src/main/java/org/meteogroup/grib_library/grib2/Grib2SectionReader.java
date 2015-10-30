e4778d07ffb4ced3a1de2a38eb59b17216c7a22b
pauw <martijn@weer.nl>
2015-10-30 14:43:45 +0100

    a bit more neat

--- src/main/java/org/meteogroup/grib_library/grib2/Grib2SectionReader.java
+++ src/main/java/org/meteogroup/grib_library/grib2/Grib2SectionReader.java
@@ -3,14 +3,23 @@ package org.meteogroup.grib_library.grib2;
 import org.meteogroup.grib_library.exception.BinaryNumberConversionException;
 import org.meteogroup.grib_library.util.BytesToPrimitiveHelper;
 
+/**
+ * 
+ * @author Pauw
+ *
+ */
 public class Grib2SectionReader {
 
+	private static final int POSITION_LENGTH_1 = 0;
+	private static final int POSITION_LENGTH_2 = 1;
+	private static final int POSITION_LENGTH_3 = 2;
+	private static final int POSITION_LENGTH_4 = 3;
 	private static final int POSITION_SECTIONNUMBER = 4;
 	
 	protected int readSectionLength(byte[] bytes, int headerOffSet) throws BinaryNumberConversionException {
 
-		return BytesToPrimitiveHelper.bytesToInteger(bytes[0], bytes[1],
-				bytes[2], bytes[3]);
+		return BytesToPrimitiveHelper.bytesToInteger(bytes[POSITION_LENGTH_1], bytes[POSITION_LENGTH_2],
+				bytes[POSITION_LENGTH_3], bytes[POSITION_LENGTH_4]);
 
 	}
 	
