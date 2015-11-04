440753b956d8f211560b4a04924deff5014ada0b
Erik Roijen <erik.roijen@meteogroup.com>
2015-11-04 14:13:19 +0100

    Removing more magic

--- src/main/java/org/meteogroup/griblibrary/grib2/Grib2RecordReader.java
+++ src/main/java/org/meteogroup/griblibrary/grib2/Grib2RecordReader.java
@@ -14,19 +14,33 @@ import org.meteogroup.griblibrary.util.BytesToPrimitiveHelper;
 public class Grib2RecordReader {
 
 	private Grib2IDSReader idsReader;
-	
+
+    private static final int GRIB_WORD_LENGTH = 4;
+    private static final int POSITION_VERSION_NUMBER = 7;
+    private static final int CORRECT_VERSION_NUMBER = 2;
+    private static final int POSITION_LENGTH_1 = 8;
+    private static final int POSITION_LENGTH_2 = 9;
+    private static final int POSITION_LENGTH_3 = 10;
+    private static final int POSITION_LENGTH_4 = 11;
+    private static final int POSITION_LENGTH_5 = 12;
+    private static final int POSITION_LENGTH_6 = 13;
+    private static final int POSITION_LENGTH_7 = 14;
+    private static final int POSITION_LENGTH_8 = 15;
+    private static final int MINIMUM_REQUIRED_LENGTH_IN_BIT = 15;
+
+
     public boolean checkIfGribFileIsValidGrib2(byte[] recordHeader) {
         String headerString = new String();
-        for (int x = 0; x < 4 ;x++) {
+        for (int x = 0; x < GRIB_WORD_LENGTH ;x++) {
             headerString = headerString + (char) recordHeader[x];
         }
-        short versionNumber = recordHeader[7];
-        return (headerString.equals("GRIB") && versionNumber == 2);
+        short versionNumber = recordHeader[POSITION_VERSION_NUMBER];
+        return (headerString.equals("GRIB") && versionNumber == CORRECT_VERSION_NUMBER);
     }
 
     public long readRecordLength(byte[] recordHeader) throws BinaryNumberConversionException, GribReaderException {
-        long length = BytesToPrimitiveHelper.bytesToLong(recordHeader[8],recordHeader[9],recordHeader[10],recordHeader[11],recordHeader[12],recordHeader[13],recordHeader[14],recordHeader[15]);
-        if (length < 15){
+        long length = BytesToPrimitiveHelper.bytesToLong(recordHeader[POSITION_LENGTH_1],recordHeader[POSITION_LENGTH_2],recordHeader[POSITION_LENGTH_3],recordHeader[POSITION_LENGTH_4],recordHeader[POSITION_LENGTH_5],recordHeader[POSITION_LENGTH_6],recordHeader[POSITION_LENGTH_7],recordHeader[POSITION_LENGTH_8]);
+        if (length < MINIMUM_REQUIRED_LENGTH_IN_BIT){
             throw new GribReaderException("The suggested length in the record header is invalid.");
         }
         return length;
