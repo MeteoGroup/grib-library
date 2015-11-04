e51d6f891e2666bee88df12f627bfd7f8b38b61e
Erik Roijen <erik.roijen@meteogroup.com>
2015-11-04 14:05:18 +0100

    Removing "magic" from horizontal level template reader

--- src/main/java/org/meteogroup/griblibrary/grib2/pdstemplates/HorizontalLevelTemplateReader.java
+++ src/main/java/org/meteogroup/griblibrary/grib2/pdstemplates/HorizontalLevelTemplateReader.java
@@ -10,28 +10,52 @@ import org.meteogroup.griblibrary.util.BytesToPrimitiveHelper;
 
 public class HorizontalLevelTemplateReader implements ProductTemplateReader {
 
-	public static final int BYTE_MASK = 0xff;
-	
-	 @Override
+	private static final int POSITION_PARAMETER_CATEGORY = 9;
+	private static final int POSITION_PARAMETER_NUMBER = 10;
+	private static final int POSITION_TYPE_OF_GENERATING_PROCESS = 11;
+	private static final int POSITION_BACKGROUND_GENERATING_PROCESS_IDENTIFIER = 12;
+	private static final int POSITION_ANALYSIS_OR_FORECAST_IDENTIFIED = 13;
+	private static final int POSITION_HOURS_OF_OBSERVATION_DATA_CUTOFF_AFTER_REFERENCE_TIME_1 = 14;
+	private static final int POSITION_HOURS_OF_OBSERVATION_DATA_CUTOFF_AFTER_REFERENCE_TIME_2 = 15;
+	private static final int POSITION_MINUTES_OF_OBSERVATION_DATA_CUTOFF_AFTER_REFERENCE_TIME = 16;
+	private static final int POSITION_IDENTICATOR_OF_UNIT_OF_TIME_RANGE = 17;
+	private static final int POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_1 = 18;
+	private static final int POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_2 = 19;
+	private static final int POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_3 = 20;
+	private static final int POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_4 = 21;
+	private static final int POSITION_TYPE_OF_FIRST_FIXED_SURFACE = 22;
+	private static final int POSITION_SCALE_FACTOR_OF_FIRST_FIXED_SURFACE = 23;
+	private static final int POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_1 = 24;
+	private static final int POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_2 = 25;
+	private static final int POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_3 = 26;
+	private static final int POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_4 = 27;
+	private static final int POSITION_TYPE_OF_SECOND_FIXED_SURFACE = 28;
+	private static final int POSITION_SCALE_FACTOR_OF_SECOND_FIXED_SURFACE = 29;
+	private static final int POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_1 = 30;
+	private static final int POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_2 = 31;
+	private static final int POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_3 = 32;
+	private static final int POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_4 = 33;
+
+	@Override
 	    public ProductTemplate readTemplate(byte[] pdsValues) throws BinaryNumberConversionException {
 	        HorizontalLevelTemplate template = new HorizontalLevelTemplate();
-	        template.setParameterCategory((short) (pdsValues[9] & BYTE_MASK));
-	        template.setParameterNumber((short) (pdsValues[10] & BYTE_MASK));
-	        template.setGeneratingProcess((short) (pdsValues[11] & BYTE_MASK));
-	        template.setBackgroundGenerating((short) (pdsValues[12] & BYTE_MASK));
-	        template.setAnalysisProcess((short) (pdsValues[13] & BYTE_MASK));
+	        template.setParameterCategory((short) (pdsValues[POSITION_PARAMETER_CATEGORY] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setParameterNumber((short) (pdsValues[POSITION_PARAMETER_NUMBER] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setGeneratingProcess((short) (pdsValues[POSITION_TYPE_OF_GENERATING_PROCESS] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setBackgroundGenerating((short) (pdsValues[POSITION_BACKGROUND_GENERATING_PROCESS_IDENTIFIER] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setAnalysisProcess((short) (pdsValues[POSITION_ANALYSIS_OR_FORECAST_IDENTIFIED] & BytesToPrimitiveHelper.BYTE_MASK));
 	        
 	               
-	        template.setHoursOfObservation(BytesToPrimitiveHelper.signedBytesToInt(pdsValues[14], pdsValues[15]));
-	        template.setMinutesOfObservation((short) (pdsValues[16] & BYTE_MASK));
-	        template.setUnitOfTimeRange( (HorizontalLevelTemplate.HorizontalLevelTimeUnit.valueOf((pdsValues[17] & BYTE_MASK))));
-	        template.setForeCastTimeInTimeRange(BytesToPrimitiveHelper.bytesToInteger(pdsValues[18], pdsValues[19], pdsValues[20], pdsValues[21]));
-	        template.setTypeOfFirstFixedSurface((short) (pdsValues[22] & BYTE_MASK));
-	        template.setScaleOfFirstFixedSurface((short) (pdsValues[23] & BYTE_MASK));
-	        template.setScaleValueOfFirstFixedSurface(BytesToPrimitiveHelper.bytesToInteger(pdsValues[24], pdsValues[25], pdsValues[26], pdsValues[27]));
-	        template.setTypeOfSecondFixedSurface((short) (pdsValues[28] & BYTE_MASK));
-	        template.setScaleOfSecondFixedSurface((short) (pdsValues[29] & BYTE_MASK));
-	        template.setScaleValueOfSecondFixedSurface(BytesToPrimitiveHelper.bytesToInteger(pdsValues[30], pdsValues[31], pdsValues[32], pdsValues[33]));
+	        template.setHoursOfObservation(BytesToPrimitiveHelper.signedBytesToInt(pdsValues[POSITION_HOURS_OF_OBSERVATION_DATA_CUTOFF_AFTER_REFERENCE_TIME_1], pdsValues[POSITION_HOURS_OF_OBSERVATION_DATA_CUTOFF_AFTER_REFERENCE_TIME_2]));
+	        template.setMinutesOfObservation((short) (pdsValues[POSITION_MINUTES_OF_OBSERVATION_DATA_CUTOFF_AFTER_REFERENCE_TIME] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setUnitOfTimeRange( (HorizontalLevelTemplate.HorizontalLevelTimeUnit.valueOf((pdsValues[POSITION_IDENTICATOR_OF_UNIT_OF_TIME_RANGE] & BytesToPrimitiveHelper.BYTE_MASK))));
+	        template.setForeCastTimeInTimeRange(BytesToPrimitiveHelper.bytesToInteger(pdsValues[POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_1], pdsValues[POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_2], pdsValues[POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_3], pdsValues[POSITION_FORECAST_TIME_IN_UNIT_AS_DEFINED_4]));
+	        template.setTypeOfFirstFixedSurface((short) (pdsValues[POSITION_TYPE_OF_FIRST_FIXED_SURFACE] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setScaleOfFirstFixedSurface((short) (pdsValues[POSITION_SCALE_FACTOR_OF_FIRST_FIXED_SURFACE] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setScaleValueOfFirstFixedSurface(BytesToPrimitiveHelper.bytesToInteger(pdsValues[POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_1], pdsValues[POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_2], pdsValues[POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_3], pdsValues[POSITION_SCALED_VALUE_OF_FIRST_FIXED_SURFACE_4]));
+	        template.setTypeOfSecondFixedSurface((short) (pdsValues[POSITION_TYPE_OF_SECOND_FIXED_SURFACE] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setScaleOfSecondFixedSurface((short) (pdsValues[POSITION_SCALE_FACTOR_OF_SECOND_FIXED_SURFACE] & BytesToPrimitiveHelper.BYTE_MASK));
+	        template.setScaleValueOfSecondFixedSurface(BytesToPrimitiveHelper.bytesToInteger(pdsValues[POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_1], pdsValues[POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_2], pdsValues[POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_3], pdsValues[POSITION_SCALED_VALUE_OF_SECOND_FIXED_SURFACE_4]));
 	        return template;
 	    }
 }
